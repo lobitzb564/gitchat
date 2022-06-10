dd6d966c47c51fab362a218bf5e3aefebd979e8f
devamlani35 <amlanid35@gmail.com>
2022-06-10 11:55:25 -0400

    New backend script: where we dropping?

--- write_message.py
+++ write_message.py
@@ -2,7 +2,7 @@ import sys
 from github import Github
 import json
 if __name__ == "__main__":
-    token = "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"
+    token = "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"
     g = Github(token)
     username = sys.argv[1]
     message  = username + ": " + sys.argv[2]
@@ -12,12 +12,17 @@ if __name__ == "__main__":
         repo.create_issue(message)
     elif script_mode == "r":
         issue_arr = list(repo.get_issues(state="open"))
-        ret_arr = []
-        for issue in issue_arr:
-            ret_arr.append(issue.title)
-        ret_dict = {"messages":ret_arr}
-        print(json.dumps(ret_dict))
-    else:
-        open_issues = repo.get_issues(state='open')
-        for issue in open_issues:
-            issue.edit(state='closed')
+        temp_arr = ["".join(i.title.split()[1:]) for i in issue_arr]
+        last_two = list(map(lambda s: s.strip().casefold(), temp_arr[:2]))
+        
+        if (len(issue_arr)>=2) and (last_two[0] == "tilted") and (last_two[1]=="wherewedropping"):    
+            for issue in issue_arr:
+                issue.edit(state='closed')
+            print("")
+        else:
+            ret_arr = []
+            for issue in issue_arr:
+                ret_arr.append(issue.title)
+            ret_dict = {"messages":ret_arr}
+            print(json.dumps(ret_dict))
+
